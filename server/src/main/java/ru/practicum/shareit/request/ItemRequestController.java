c7a815d12ea941ea993711ca6dad200d98ee6b08
apavlov <pavlow.andrei@gmail.com>
2023-02-08 10:00:02 +0300

    fix: unused import removed

--- server/src/main/java/ru/practicum/shareit/request/ItemRequestController.java
+++ server/src/main/java/ru/practicum/shareit/request/ItemRequestController.java
@@ -2,7 +2,6 @@ package ru.practicum.shareit.request;
 
 import java.util.List;
 
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
